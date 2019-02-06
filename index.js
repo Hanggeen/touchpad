6b635344f44321a216e1a670addc8aa8c2c47830
Hanggeen <592499197@qq.com>
2019-02-06 22:42:18 +0800

    更新服务端代码，按类型处理数据

--- index.js
+++ index.js
@@ -5,41 +5,72 @@ const WebSocket = require('ws');
 const WebSocketServer = WebSocket.Server;
 
 // 实例化:
-const wss1 = new WebSocketServer({
+const wss = new WebSocketServer({
     port: 3000
 });
-// 实例化:
-const wss2 = new WebSocketServer({
-    port: 3001
-});
 
-let window = {}
+let STORE = {}
 
-wss1.on('connection', function (ws) {
-  console.log(`trackpad connection()`);
+wss.on('connection', function (ws) {
+  console.log('有新连接');
+  let code = '';
   ws.on('message', function (message) {
-      console.log(`trackpad 收到: ${message}`);
-      if (window.screen) {
-          window.screen.send(message)
+    let msg;
+    try {
+      msg = JSON.parse(message)
+    } catch (e) {
+      ws.send('parse error')
+    }
+    if (msg.type == 'init-listener') {
+      if (STORE[msg.data]) {
+        STORE[msg.data].listener = ws;
+        code = msg.data;
+      } else {
+        STORE[msg.data] = {
+          listener: ws
+        }
       }
-    //   ws.send(`ECHO: ${message}`, (err) => {
-    //       if (err) {
-    //           console.log(`[SERVER] error: ${err}`);
-    //       }
-    //   });
+      ws.send('注册成功')
+    }
+    else if (msg.type == 'init-poster') {
+      if (STORE[msg.data]) {
+        STORE[msg.data].poster = ws;
+        code = msg.data;
+      } else {
+        STORE[msg.data] = {
+          poster: ws
+        }
+      }
+      ws.send('注册成功')
+    }
+    else if (STORE[msg.code]) {
+
+      if (msg.type == 'post') {
+        if (STORE[msg.code].listener != undefined) {
+          STORE[msg.code].listener.send(message)
+        } else {
+          ws.send('unregisted1')
+        }
+      }
+
+      if (msg.type == 'listen') {
+        if (STORE[msg.code].post) {
+          STORE[msg.code].post.send(message)
+        } else {
+          ws.send('unregisted2')
+        }
+      }
+    } else {
+      ws.send('unregisted3')
+    }
+  });
+
+  ws.on('close', function () {
+    if (code) {
+      STORE['code'] = null;
+    }
   })
+
 });
 
-wss2.on('connection', function (ws) {
-    console.log(`[screen] connection()`);
-    ws.on('message', function (message) {
-        console.log(`[screen] Received: ${message}`);
-        // ws.send(`ECHO: ${message}`, (err) => {
-        //     if (err) {
-        //         console.log(`[screen] error: ${err}`);
-        //     }
-        // });
-        window.screen = ws;
-    })
-  });
-  
+console.log('ready')
