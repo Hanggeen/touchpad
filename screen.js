a4d80f5ce7dcbe20ba7cc11e4f3d591b8eb7e970
Hanggeen <592499197@qq.com>
2019-02-13 20:45:18 +0800

    添加鼠标图标

--- screen.js
+++ screen.js
@@ -1,6 +1,6 @@
 // 打开一个WebSocket:
 var point = document.createElement('div');
-point.setAttribute('style',  "position:fixed;width:5px;height:5px;background-color:#000;left:0px;top:0px;")
+point.setAttribute('style',  "position:fixed;width:11px;height:17px;left:0px;top:0px;background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAARBAMAAAD5z0voAAAAMFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD///9agToZAAAADnRSTlMADDx3JLOnYDAYm4NrSMnfdYcAAABcSURBVAjXY+CfyAAC/z4JgKn3imDqHZALooBcEAXkgiggF0SBuGDqvQKI+v+tAKjv3dfFQN7Xf+9PCDD4Z/97b8LAIFTU/+6vAAPjRPl3nx2A5rD8DwZRjMpGAgBjcjVBzNw01wAAAABJRU5ErkJggg==)")
 
 document.getElementsByTagName('body')[0].appendChild(point);
 
@@ -24,7 +24,7 @@ ws.addEventListener('message', function (message) {
     if (msg.action == 'click') {
       console.log(Math.floor(X), Math.floor(Y))
       console.log(document.elementFromPoint(Math.floor(X), Math.floor(Y)))
-      document.elementFromPoint(Math.floor(X)-6, Math.floor(Y)-6).click()
+      document.elementFromPoint(Math.floor(X)-1, Math.floor(Y)-1).click()
     }
     if (msg.action == 'scroll') {
       let height = document.body.scrollTop || document.documentElement.scrollTop;
