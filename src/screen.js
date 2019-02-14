78bb562a86478dadd3cc76bb4e54c18f064f8dcf
Hanggeen <592499197@qq.com>
2019-02-14 18:45:15 +0800

    整理目录，区分服务和页面，添加类

---
+++ src/screen.js
@@ -0,0 +1,50 @@
+class RemoveReceiver {
+  constructor(url = '127.0.0.1:3000') {
+    this.wsurl = url
+    this._initPointer()
+    this._initWebSocket()
+  }
+  _initPointer() {
+    this.pointerX = 0;
+    this.pointerY = 0;
+    if (document.getElementById('_Pointer') == null) {
+      var pointer = document.createElement('div');
+      pointer.setAttribute('id', '_Pointer');
+      pointer.setAttribute('style',  "position:fixed;width:11px;height:17px;left:0px;top:0px;background-image:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAARBAMAAAD5z0voAAAAMFBMVEUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD///9agToZAAAADnRSTlMADDx3JLOnYDAYm4NrSMnfdYcAAABcSURBVAjXY+CfyAAC/z4JgKn3imDqHZALooBcEAXkgiggF0SBuGDqvQKI+v+tAKjv3dfFQN7Xf+9PCDD4Z/97b8LAIFTU/+6vAAPjRPl3nx2A5rD8DwZRjMpGAgBjcjVBzNw01wAAAABJRU5ErkJggg==)")
+      document.getElementsByTagName('body')[0].appendChild(pointer);
+      this.pointer = pointer
+    }
+  }
+  _initWebSocket() {
+    let ws = new WebSocket(`ws://${this.wsurl}`);
+    // 打开WebSocket连接后立刻发送一条消息:
+    ws.addEventListener('open', function () {
+        ws.send(JSON.stringify({
+          type: 'init-listener',
+          data: 'first'
+        }));
+    });
+    // 响应收到的消息:
+    ws.addEventListener('message', message => {
+        var msg = JSON.parse(message.data);
+        if (msg.action == 'slide') {
+          this._setPointer(msg.data.x, msg.data.y);
+        }
+        if (msg.action == 'click') {
+          document.elementFromPoint(Math.floor(this.pointerX)-1, Math.floor(this.pointerY)-1).click()
+        }
+        if (msg.action == 'scroll') {
+          let height = document.body.scrollTop || document.documentElement.scrollTop;
+          height -= Math.round(msg.data.y * 3);
+          document.body.scrollTop = document.documentElement.scrollTop = height;
+          console.log(msg.data.x, msg.data.y);
+        }
+    })
+  }
+  _setPointer(x, y) {
+    this.pointerX += x;
+    this.pointerY += y;
+    this.pointer.style.left = this.pointerX + 'px';
+    this.pointer.style.top = this.pointerY + 'px';
+  }
+}
---
+++ src/trackpad.js
@@ -0,0 +1,191 @@
+// 打开一个WebSocket:
+var ws = new WebSocket("ws://10.13.131.182:3000");
+
+var CODE = "first";
+var MODE = 'gesture'
+var padDom = document.getElementById("pad");
+var barDom = document.getElementById("bar");
+var mouseDom = document.getElementById("mouse");
+var gestureDom = document.getElementById("gesture");
+
+var padStartX = 0, padStartY = 0;
+var barStartX = 0, barStartY = 0;
+
+var hammertime = new Hammer(padDom, {});
+
+// 打开WebSocket连接后立刻发送一条消息:
+ws.addEventListener("open", function() {
+  ws.send(
+    JSON.stringify({
+      type: "init-poster",
+      data: CODE
+    })
+  );
+});
+
+// 响应收到的消息:
+ws.addEventListener("message", function(msg) {
+  showTips(JSON.parse(msg.data).msg)
+});
+
+
+mouseDom.onclick = function () {
+  MODE = 'mouse'
+  gestureDom.classList.remove('active')
+  mouseDom.classList.add('active')
+  init()
+}
+gestureDom.onclick = function () {
+  MODE = 'gesture'
+  gestureDom.classList.add('active')
+  mouseDom.classList.remove('active')
+  init()
+}
+
+
+// 发送事件系列
+
+function sendMouseMove(x, y) {
+  ws.send(
+    JSON.stringify({
+      type: "post",
+      code: CODE,
+      action: "slide",
+      data: {
+        x: x,
+        y: y
+      }
+    })
+  );
+}
+
+function sendScroll(x, y) {
+  ws.send(
+    JSON.stringify({
+      type: "post",
+      code: CODE,
+      action: "scroll",
+      data: {
+        x: x,
+        y: y
+      }
+    })
+  );
+}
+
+function sendClick() {
+  ws.send(
+    JSON.stringify({
+      type: "post",
+      code: CODE,
+      action: "click",
+      data: {}
+    })
+  );
+}
+
+function sendSwipe(side) {
+  ws.send(
+    JSON.stringify({
+      type: "post",
+      code: CODE,
+      action: "swipe",
+      data: side
+    })
+  );
+}
+
+
+// 触发事件系列
+function padTouchStart(e) {
+  e.preventDefault()
+  padStartX = e.touches[0].pageX;
+  padStartY = e.touches[0].pageY;
+}
+
+function padTouchMove (e) {
+  showTips('滑动光标');
+  var X, Y;
+  X = e.touches[0].pageX - padStartX;
+  Y = e.touches[0].pageY - padStartY;
+  padStartX = e.touches[0].pageX;
+  padStartY = e.touches[0].pageY;
+  sendMouseMove(X, Y);
+}
+
+function padClick() {
+  sendClick()
+}
+
+function swipeLeft(ev) {
+  sendSwipe('left');
+  showTips('向左滑动');
+}
+
+function swipeRight(ev) {
+  sendSwipe('right');
+  showTips('向右滑动');
+}
+
+function swipeUp(ev) {
+  sendSwipe('up');
+  showTips('向上滑动');
+}
+
+function swipeDown(ev) {
+  sendSwipe('down');
+  showTips('向下滑动');
+}
+
+// 公用
+function showTips(text) {
+  document.getElementById("tips").innerHTML = text;
+  document.getElementById("tips").classList.remove("hide");
+  clearTimeout(window.tipsTimeout);
+  window.tipsTimeout = setTimeout(function() {
+    document.getElementById("tips").classList.add("hide");
+  }, 2000);
+}
+
+
+
+
+// 滚动条添加事件
+barDom.addEventListener("touchstart", function(e) {
+  barStartX = e.touches[0].pageX;
+  barStartY = e.touches[0].pageY;
+});
+barDom.addEventListener("touchmove", function(e) {
+    showTips('滑动滚轮');
+    var X, Y;
+    X = e.touches[0].pageX - barStartX;
+    Y = e.touches[0].pageY - barStartY;
+    barStartX = e.touches[0].pageX;
+    barStartY = e.touches[0].pageY;
+    sendScroll(X, Y);
+  },
+  false
+);
+
+function init () {
+  if (MODE == 'mouse') {
+    hammertime.stop()
+    padDom.addEventListener("touchstart", padTouchStart);
+    padDom.addEventListener("touchmove", padTouchMove);
+    hammertime.on('tap', padClick);
+    hammertime.off('swipeleft', swipeLeft);
+    hammertime.off('swiperight', swipeRight);
+    hammertime.off('swipeup', swipeUp);
+    hammertime.off('swipedown', swipeDown);    
+  } else {
+    padDom.removeEventListener("touchstart", padTouchStart);
+    padDom.removeEventListener("touchmove", padTouchMove);
+    hammertime.off('tap', padClick);
+    hammertime.on('swipeleft', swipeLeft);
+    hammertime.on('swiperight', swipeRight);
+    hammertime.on('swipeup', swipeUp);
+    hammertime.on('swipedown', swipeDown);    
+  }
+}
+
+init();
