0e2d2eae133295988fa527a3f80031ddc6ed94d4
Hanggeen <592499197@qq.com>
2019-02-22 11:59:35 +0800

    鼠标模式居中显示，限定在边界内滑动

--- src/trackpad-listener/modules/pointer.js
+++ src/trackpad-listener/modules/pointer.js
@@ -4,8 +4,12 @@ export default class Pointer {
   constructor () {
     this.dom = str2dom(pointerhtml)[0]
     document.getElementsByTagName('body')[0].appendChild(this.dom)
-    this.pointerX = 0;
-    this.pointerY = 0;
+    this.clientHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    this.clientWidth = document.documentElement.clientWidth || document.body.clientWidth;
+    this.dom.style.left = this.clientWidth/2 + 'px'
+    this.dom.style.top = this.clientHeight/2 + 'px'
+    this.pointerX = this.clientWidth/2
+    this.pointerY = this.clientHeight/2
   }
   show() {
     this.dom.style.display = 'block'
@@ -16,6 +20,18 @@ export default class Pointer {
   set (x, y) {
     this.pointerX += x;
     this.pointerY += y;
+    if (this.pointerX + 11 > this.clientWidth) {
+      this.pointerX = this.clientWidth - 11
+    }
+    if (this.pointerY + 11  > this.clientHeight) {
+      this.pointerY = this.clientHeight - 11
+    }
+    if (this.pointerX < 0) {
+      this.pointerX = 0
+    }
+    if (this.pointerY < 0) {
+      this.pointerY = 0
+    }
     this.dom.style.left = this.pointerX + 'px';
     this.dom.style.top = this.pointerY + 'px';
   }
